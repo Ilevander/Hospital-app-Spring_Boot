9a7077b596e516f1f36c676e16604419dd473405
dell <dell@DESKTOP-4P8U8AR>
2024-05-09 22:40:53 +0100

    creating packages

---
+++ src/test/java/com/elamri/Hospitalapp/HospitalAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elamri.Hospitalapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
