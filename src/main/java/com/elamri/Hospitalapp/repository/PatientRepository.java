dcca21cd6812396669cf2b9392326295eb18b815
dell <dell@DESKTOP-4P8U8AR>
2024-05-10 01:27:29 +0100

    coding Delete method in controller Patient

--- src/main/java/com/elamri/Hospitalapp/repository/PatientRepository.java
+++ src/main/java/com/elamri/Hospitalapp/repository/PatientRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.elamri.Hospitalapp.entities.Patient;
 
+//Interface for business Logic and DAO Layer
 public interface PatientRepository extends JpaRepository<Patient, Long>{
 
 }
