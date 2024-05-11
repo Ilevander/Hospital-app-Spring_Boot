a0b24d1944b040df6fc4de1297472a8077e7efb1
dell <dell@DESKTOP-4P8U8AR>
2024-05-11 16:08:07 +0100

    Pagination Option using PageRequest

--- src/main/java/com/elamri/Hospitalapp/web/PatientController.java
+++ src/main/java/com/elamri/Hospitalapp/web/PatientController.java
@@ -3,6 +3,8 @@ package com.elamri.Hospitalapp.web;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,15 +20,15 @@ public class PatientController {
 	private PatientRepository patientRepository;
 	
 	@GetMapping("/index")
-	public String index(Model model) {
-		List<Patient> patients = patientRepository.findAll();
+	public String index(Model model , int page , int size) {
+		Page<Patient> patients = patientRepository.findAll(PageRequest.of(page, size));
 		model.addAttribute("listPatients",patients);
 		return "patients";
 	}
 	
 	@GetMapping("/deletePatient")
 	public String delete(@RequestParam(name = "id")Long id) {
-		patientRepository.deleteById(id);
+		patientRepository.deleteById(id); 
 		return "redirect:/index";
 	}
 
