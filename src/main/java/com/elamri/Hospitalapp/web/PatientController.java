b466038d5c2687c69901a4eb42ce05815167ed43
dell <dell@DESKTOP-4P8U8AR>
2024-05-11 16:15:25 +0100

    Patient Controller Pagination

--- src/main/java/com/elamri/Hospitalapp/web/PatientController.java
+++ src/main/java/com/elamri/Hospitalapp/web/PatientController.java
@@ -20,9 +20,13 @@ public class PatientController {
 	private PatientRepository patientRepository;
 	
 	@GetMapping("/index")
-	public String index(Model model , int page , int size) {
-		Page<Patient> patients = patientRepository.findAll(PageRequest.of(page, size));
-		model.addAttribute("listPatients",patients);
+	public String index(Model model , 
+			            @RequestParam( name="page" , defaultValue = "0") int page,
+			            @RequestParam(name="size" , defaultValue = "5") int size ) {
+		Page<Patient> pagePatients = patientRepository.findAll(PageRequest.of(page, size));
+		model.addAttribute("listPatients",pagePatients.getContent());
+		model.addAttribute("pages",new int[pagePatients.getTotalPages()]);
+		model.addAttribute("currentPage",page);
 		return "patients";
 	}
 	
