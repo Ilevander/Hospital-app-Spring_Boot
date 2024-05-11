c3bd4211ec725c22be2077ff5c6d704f0a804865
dell <dell@DESKTOP-4P8U8AR>
2024-05-11 16:57:12 +0100

    Injecting Pageable Object in Repository DAO layer

--- src/main/java/com/elamri/Hospitalapp/repository/PatientRepository.java
+++ src/main/java/com/elamri/Hospitalapp/repository/PatientRepository.java
@@ -1,10 +1,13 @@
 package com.elamri.Hospitalapp.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.elamri.Hospitalapp.entities.Patient;
 
 //Interface for business Logic and DAO Layer
 public interface PatientRepository extends JpaRepository<Patient, Long>{
-
+	Page<Patient> findByFirstNameContainsIgnoreCase(String keyword, Pageable pageRequest);
 }
