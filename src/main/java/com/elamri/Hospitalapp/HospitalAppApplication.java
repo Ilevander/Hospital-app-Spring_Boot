10f3044b9def29a8738a2dcad2282353fb3526eb
dell <dell@DESKTOP-4P8U8AR>
2024-05-16 10:36:23 +0100

    Password Encoder Bean

--- src/main/java/com/elamri/Hospitalapp/HospitalAppApplication.java
+++ src/main/java/com/elamri/Hospitalapp/HospitalAppApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.elamri.Hospitalapp.entities.Patient;
 import com.elamri.Hospitalapp.repository.PatientRepository;
@@ -55,6 +57,10 @@ public class HospitalAppApplication {
 			
 			
 		};
+	}
+	
+	PasswordEncoder passwordEncoder(){
+		return new BCryptPasswordEncoder();
 	}
 
 }
