7e5793de0ef2b38beb73ba250b471395b8bbb926
dell <dell@DESKTOP-4P8U8AR>
2024-05-16 13:23:14 +0100

    authorizeHttpRequests() -> autorizeRequests();

--- src/main/java/com/elamri/Hospitalapp/security/SecurityConfig.java
+++ src/main/java/com/elamri/Hospitalapp/security/SecurityConfig.java
@@ -12,29 +12,25 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-
 public class SecurityConfig {
-	
-	@Autowired
-	private PasswordEncoder passwordEncoder;
-	
-	@Bean
-	public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
-		return new InMemoryUserDetailsManager(
-				//{noop} to not compare passwords to PasswordEncoder
-				//User.withUsername("user1").password("{noop}123").roles("USER").build(),
-				User.withUsername("user1").password(passwordEncoder.encode("user1")).roles("USER").build(),
-				User.withUsername("user2").password(passwordEncoder.encode("user2")).roles("USER").build(),
-				User.withUsername("admin").password(passwordEncoder.encode("admin")).roles("USER","ADMIN").build()
-				);
-	}
-	
-	@SuppressWarnings("removal")
-	@Bean
-	public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
-		httpSecurity.formLogin();
-		httpSecurity.authorizeHttpRequests().anyRequest().authenticated();
-		return httpSecurity.build();
-	}
+    
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    
+    @Bean
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+        return new InMemoryUserDetailsManager(
+                User.withUsername("user1").password(passwordEncoder.encode("user1")).roles("USER").build(),
+                User.withUsername("user2").password(passwordEncoder.encode("user2")).roles("USER").build(),
+                User.withUsername("admin").password(passwordEncoder.encode("admin")).roles("USER","ADMIN").build()
+        );
+    }
+    
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+        httpSecurity.formLogin();
+        httpSecurity.authorizeRequests().anyRequest().authenticated();
+        return httpSecurity.build();
+    }
 
 }
