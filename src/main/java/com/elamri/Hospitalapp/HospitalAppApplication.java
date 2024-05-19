b3f26c76236ff9636134821432af42128c21bd8f
dell <dell@DESKTOP-4P8U8AR>
2024-05-19 21:00:20 +0100

    Creating Users , Authorities tables from code first using JdbcUserDetailsManager As Bean Spring Security

--- src/main/java/com/elamri/Hospitalapp/HospitalAppApplication.java
+++ src/main/java/com/elamri/Hospitalapp/HospitalAppApplication.java
@@ -9,6 +9,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import com.elamri.Hospitalapp.entities.Patient;
 import com.elamri.Hospitalapp.repository.PatientRepository;
@@ -58,6 +61,37 @@ public class HospitalAppApplication {
 			
 		};
 	}
+	
+	@Bean
+    CommandLineRunner commandLineRunner(JdbcUserDetailsManager jdbcUserDetailsManager, PasswordEncoder passwordEncoder) {
+        return args -> {
+            UserDetails user1 = User.withUsername("user11")
+                    .password(passwordEncoder.encode("user11"))
+                    .roles("USER")
+                    .build();
+
+            UserDetails user2 = User.withUsername("user22")
+                    .password(passwordEncoder.encode("user22"))
+                    .roles("USER")
+                    .build();
+
+            UserDetails admin = User.withUsername("admin2")
+                    .password(passwordEncoder.encode("admin2"))
+                    .roles("USER", "ADMIN")
+                    .build();
+
+            if (!jdbcUserDetailsManager.userExists(user1.getUsername())) {
+                jdbcUserDetailsManager.createUser(user1);
+            }
+            if (!jdbcUserDetailsManager.userExists(user2.getUsername())) {
+                jdbcUserDetailsManager.createUser(user2);
+            }
+            if (!jdbcUserDetailsManager.userExists(admin.getUsername())) {
+                jdbcUserDetailsManager.createUser(admin);
+            }
+        };
+    }  
+	
 	
 	@Bean
 	PasswordEncoder passwordEncoder(){
--- src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
--- SQL scripts to add to the data base like insert into, alter , select to the database related:
-
-insert into users(username, password, enabled) values ('user1', '123', true);
