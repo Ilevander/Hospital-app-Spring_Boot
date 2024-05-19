935e13628c9ca0d90fc468dfe68632afd5d45ed5
dell <dell@DESKTOP-4P8U8AR>
2024-05-19 15:33:38 +0100

    defaultSuccessUrl(/) - Spring Security

--- src/main/java/com/elamri/Hospitalapp/security/SecurityConfig.java
+++ src/main/java/com/elamri/Hospitalapp/security/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
-            .formLogin().loginPage("/login").permitAll()
+            .formLogin().loginPage("/login").defaultSuccessUrl("/").permitAll()
             .and()
             .rememberMe()
             .and()
